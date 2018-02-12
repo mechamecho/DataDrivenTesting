3499f01db840198fbf757e34d43f81487c675502
Livepurposefully <nafissa_92@hotmail.com>
2018-02-11 21:36:18 -0500

    Add static methods to simplify the conversion of the JSONFile into a JObject to use in constructors of the JRONReader class.

--- TestDataAccess/JSONReader.cs
+++ TestDataAccess/JSONReader.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.IO;
 
 namespace TestDataAccess
 {
@@ -9,5 +12,47 @@ namespace TestDataAccess
         public static Dictionary<string, string> TestCaseValues;
         public static int ListCount;
 
+
+        /// <summary>
+        /// Constructor for a JSONReader to read the testCaseValues from a 
+        /// JSON File
+        /// </summary>
+        /// <param name="directoryPath">Directory of the JSON file</param>
+        /// <param name="jsonFileName">Name of the JSON file</param>
+        /// <param name="testDataKey"></param>
+        /// <param name="testDataIndex"></param>
+        public JSONReader(string directoryPath, string jsonFileName, string testDataKey, int testDataIndex)
+        {
+
+        }
+
+        private static JObject ConvertFileToJObject(string directoryPath, string jsonFileName)
+        {
+            using (StreamReader file = File.OpenText(adjustJSONFilePath(directoryPath, jsonFileName)))
+            {
+                using (JsonTextReader reader = new JsonTextReader(file))
+                {
+                    return (JObject)JToken.ReadFrom(reader);
+                }
+            }
+            {
+
+            }
+        }
+
+        private static string adjustJSONFilePath(string directoryPath, string JSONFileName)
+        {
+            string corrdirectoryPath = directoryPath +
+                (JSONFileName.Contains("QA Automation\\") ? "" : "\\QA Automation\\") +
+                (JSONFileName.Contains("TestCases\\") ? "" : (JSONFileName.Contains("TestCase\\") ?
+                "" : (JSONFileName.Contains("TestData\\") ? "" : "TestData\\"))) +
+                JSONFileName;
+
+            return corrdirectoryPath;
+        }
+
+
+
     }
+
 }
